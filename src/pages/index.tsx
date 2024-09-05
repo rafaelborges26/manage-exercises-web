f7cd24bef68128489850ba50f5779e6cc9823792
Rafael Borges <rafael.borges2698@gmail.com>
2024-09-05 02:28:46 -0300

    feat - created page home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,68 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
-import { Button } from "@/components/ui/button";
-import { Header } from "@/components/header";
+import { Inter } from 'next/font/google'
+import Image from 'next/image'
+import { useRouter } from 'next/router'
 
-const inter = Inter({ subsets: ["latin"] });
+import FitnessModelsImage from '@/assets/images/FitnessModels.png'
+import GroupImage from '@/assets/images/Group.png'
+import { Header } from '@/components/header'
+import { Button } from '@/components/ui/button'
+
+const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const router = useRouter()
+
+  function handleNavigateSignIn() {
+    router.push('/sign-in')
+  }
+
+  function handleNavigateSignUp() {
+    router.push('/sign-up')
+  }
+
   return (
-    <div className="min-h-screen h-auto antialiased px-6 flex">
-      <section className="flex flex-col justify-center">
+    <div className="flex h-auto min-h-screen px-6 antialiased">
+      <section className="flex w-[75%] flex-col justify-center md:w-[45%]">
         <span className="text-[2rem] font-bold">
-        <strong className="text-green-500">Lorem ipsum</strong> dolor sit amet. 
+          <strong className="text-green-600 dark:text-green-500">
+            PersonalPro:
+          </strong>{' '}
+          Gestão de Treinos e Aulas
         </span>
-        
+
         <span className="text-[2rem] font-bold">
-        <strong className="text-green-500">Lorem ipsum</strong> dolor sit amet. 
+          <strong className="text-green-600 dark:text-green-500">
+            TreinoMaster:
+          </strong>{' '}
+          Controle Completo de Aulas e Treinos
         </span>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto rem itaque tenetur ut eos vel aut beatae soluta, accusamus aliquam placeat doloremque est labore iure! Ex possimus temporibus optio doloribus?</p>
+        <p className="mt-4 text-sm font-medium text-gray-700 dark:text-gray-400">
+          O PersonalPro é a solução ideal para personal trainers organizarem e
+          acompanharem os treinos e aulas de seus alunos de forma prática e
+          eficiente.
+        </p>
+        <p className="text-sm font-medium text-gray-700 dark:text-gray-400">
+          Com ferramentas intuitivas, você pode criar planos de treino
+          personalizados, monitorar o desempenho, agendar aulas e otimizar o
+          progresso dos seus alunos. Simplifique sua rotina e ofereça um serviço
+          ainda mais profissional com o PersonalPro.
+        </p>
+        <div className="mb-2 mt-4 flex flex-row gap-4">
+          <Button
+            onClick={handleNavigateSignUp}
+            className="bg-green-500 font-bold hover:bg-green-600"
+          >
+            Criar conta
+          </Button>
+          <Button onClick={handleNavigateSignIn} className="font-bold">
+            Entrar
+          </Button>
+        </div>
       </section>
-      <div>
-        img
+      <div className="m-auto h-auto w-auto">
+        <Image src={GroupImage} className="ml-5rem mb-[-10rem]" alt="" />
+        <Image src={FitnessModelsImage} alt="Fitness" />
       </div>
     </div>
-  );
+  )
 }
--- src/pages/sign-up.tsx
+++ src/pages/sign-up.tsx
@@ -1,15 +1,15 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
-import { Button } from "@/components/ui/button";
-import { Header } from "@/components/header";
+import { Inter } from 'next/font/google'
+import Image from 'next/image'
 
-const inter = Inter({ subsets: ["latin"] });
+import { Header } from '@/components/header'
+import { Button } from '@/components/ui/button'
+
+const inter = Inter({ subsets: ['latin'] })
 
 export default function SignUp() {
   return (
-    <div className="flex min-h-screen flex-col antialiased px-6">
-      <Header />
+    <div className="flex min-h-screen flex-col px-6 antialiased">
       <h5>signUp</h5>
     </div>
-  );
+  )
 }
